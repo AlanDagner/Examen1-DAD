43dd559cb104f611767ab7c7be4cd069ce50a7b2
Alan Dagner <64110677+AlanDagner@users.noreply.github.com>
2022-04-27 09:56:31 -0500

    Examen1-AlanQuenaya

---
+++ src/main/java/pregunta3/Consumidor.java
@@ -0,0 +1,19 @@
+package pregunta3;
+import pregunta3.Cola;
+
+public class Consumidor implements Runnable{
+        Cola colaCompartida;
+        public Consumidor(Cola cola){
+                this.colaCompartida=cola;
+        }
+        @Override
+        public void run() {
+                int num;
+                while (true){
+                        num=colaCompartida.desencolar();
+                        if (num!=-1){
+                                System.out.println("Consumidor recuperó el numero:"+num);
+                        } /* Fin del if*/
+                } /*Fin del bucle infinito*/
+        } /* Fin del run()*/
+} /*Fin de la clase Consumidor*/
